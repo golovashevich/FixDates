2aaddf7fa4f23522bd36a75001441c947f43de18
Maxim Golovashevich <mgolovashevych@ancestry.com>
2019-04-27 15:38:37 +0300

    Fixing file stamps

--- FixDates/Program.cs
+++ FixDates/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text.RegularExpressions;
 
 namespace FixDates
 {
@@ -19,101 +18,41 @@ namespace FixDates
                 Console.WriteLine($"Directory {sourcePath} does not exist");
                 return;
             }
-            var recognizedCount = 0;
+
+            var parser = new TimeStampParser();
             var files = Directory.GetFiles(sourcePath);
+
+            var stamps = new List<Tuple<string, DateTime>>();
             foreach (var filePath in files) {
-                if (ProcessFile(filePath)) {
-                    recognizedCount++;
+                var fileName = Path.GetFileName(filePath);
+                if (!parser.RecognizeStampFormat(fileName, out DateTime fileStamp)) {
+                   throw new InvalidOperationException($"Unrecognized {fileName}");
+                } 
+                Console.WriteLine("{0} - {1}", fileName, TimeStampParser.FileStampFromDate(fileStamp));
+                stamps.Add(Tuple.Create(filePath, fileStamp));
+            }
+
+            Console.WriteLine("All stamps are recognized");
+
+            var correctedFiles = 0;
+            foreach (var fileNameStamp in stamps) {
+                var fileTime = File.GetLastWriteTime(fileNameStamp.Item1);
+                if (fileTime != fileNameStamp.Item2) {
+                    Console.WriteLine($"{fileNameStamp.Item1} has wrong stamp {fileTime}");
+                    File.SetLastWriteTime(fileNameStamp.Item1, fileNameStamp.Item2);
+                    correctedFiles++;
                 }
             }
-
-            Console.WriteLine($"Sequences recognized: {recognizedCount} ({files.Length})");
-            Console.WriteLine("Press Enter to exit...");
+            Console.WriteLine($"{correctedFiles}/{files.Length} had wrong timestamp and were corrected");
         }
 
-        const string TIMESTAMP_MASK = 
-            @"(?<y>[1-2]\d{3})-?(?<month>[0-1]\d)-?(?<d>[0-3]\d)[_|-](?<h>[0-2]\d)-?(?<m>[0-5]\d)-?(?<s>[0-5]\d)";
-
-        const string GUID_MASK = 
-            @"[0-9a-fA-F]{{8}}-[0-9a-fA-F]{{4}}-[0-9a-fA-F]{{4}}-[0-9a-fA-F]{{4}}-[0-9a-fA-F]{{12}}";
-
-        private static string[] MASKS = {
-            "VID_{0}.mp4", 
-            "IMG_{0}.jpg",
-            @"IMG_\d\d\d\d_{0}\d{{3}}.MOV",
-            "PANO_{0}.jpg",
-            @"PH{0}-\d{{3}}.JPG",
-            "VID_{0}.3gp", 
-            "IMG_{0}_HDR.jpg",
-            @"IMG_{0}_\d.jpg",
-            @"IMG_{0}_\d{{3}}.jpg",
-            @"IMG_{0}_BURST\d.jpg",
-            @"\d{{10}}_{0}\d{{3}}.mp4",
-            String.Format(@"{0}_{{0}}\d{{{{3}}}}.mp4", GUID_MASK),
-            @"Screenshot_{0}-\d{{3}}_.*.png",
-        };
-
-        public static bool RecognizeFormat(string fileName, out DateTime fileDate) {
-            // TODO: Extract this into separate method
-            var sequences = new List<Tuple<string, Regex>>();
-            foreach(var mask in MASKS) {
-                var tempResult = String.Format(mask, TIMESTAMP_MASK);
-                sequences.Add(Tuple.Create(mask, new Regex(String.Format(mask, TIMESTAMP_MASK))));
-            }
-
-            fileDate = DateTime.MinValue;
-            foreach (var sequence in sequences) {
-                var match = sequence.Item2.Match(fileName);
-                if (match.Success)
-                {
-                    fileDate = ComposeDate(match);
-                    // CheckDateAgainstName(fileName, sequence.Item1, fileTime);
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        private static DateTime ComposeDate(Match match)
-        {
-            var year = Convert.ToInt32(match.Groups["y"].Value);
-            var month = Convert.ToInt32(match.Groups["month"].Value);
-            var day = Convert.ToInt32(match.Groups["d"].Value);
-            var hour = Convert.ToInt32(match.Groups["h"].Value);
-            var minute = Convert.ToInt32(match.Groups["m"].Value);
-            var second = Convert.ToInt32(match.Groups["s"].Value);
-
-            var fileTime = new DateTime(year, month, day, hour, minute, second);
-            return fileTime;
-        }
-
-        private static void CheckDateAgainstName(string fileName, string mask, DateTime fileTime)
-        {
-            var stampFormat = 
-                $"{fileTime.Year}{fileTime.Month:d2}{fileTime.Day:d2}_{fileTime.Hour:d2}{fileTime.Minute:d2}{fileTime.Second:d2}";
-            var checkName = String.Format(mask, stampFormat);
-
-            if (checkName != fileName)
-            {
-                throw new InvalidDataException($"Parsed name does not match original name: {fileName} - {checkName}");
-            }
-        }
-
-        const string STAMP_FORMAT = "{0}{1:d2}{2:d2}_{3:d2}{4:d2}{5:d2}"; 
-        
-        private static string FileStampFromDate(DateTime fileTime) {
-            return String.Format(STAMP_FORMAT, fileTime.Year, fileTime.Month, fileTime.Day, 
-                fileTime.Hour, fileTime.Minute, fileTime.Second);
-        }
-
-        public static bool ProcessFile(string filePath) {
+        public static bool ProcessFile(string filePath, TimeStampParser parser) {
             var fileName = Path.GetFileName(filePath);
-            if (!RecognizeFormat(fileName, out DateTime fileStamp)) {
-                    throw new InvalidOperationException($"Unrecognized {fileName}");
-                // return false;
-            } else {
-                Console.WriteLine("{0} - {1}", fileName, FileStampFromDate(fileStamp));
-            }
+            if (!parser.RecognizeStampFormat(fileName, out DateTime fileStamp)) {
+                throw new InvalidOperationException($"Unrecognized {fileName}");
+            } 
+            Console.WriteLine("{0} - {1}", fileName, TimeStampParser.FileStampFromDate(fileStamp));
+
             return true;
         }
     }
---
+++ FixDates/TimeStampParser.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace FixDates
+{
+    public class TimeStampParser
+    {
+        public const string TIMESTAMP_MASK = 
+            @"(?<y>[1-2]\d{3})-?(?<month>[0-1]\d)-?(?<d>[0-3]\d)[_|-](?<h>[0-2]\d)-?(?<m>[0-5]\d)-?(?<s>[0-5]\d)";
+
+        public const string GUID_MASK = 
+            @"[0-9a-fA-F]{{8}}-[0-9a-fA-F]{{4}}-[0-9a-fA-F]{{4}}-[0-9a-fA-F]{{4}}-[0-9a-fA-F]{{12}}";
+
+        public const string TIMESTAMP_FORMAT = "{0}{1:d2}{2:d2}_{3:d2}{4:d2}{5:d2}"; 
+        private static string[] MASKS = {
+            "VID_{0}.mp4", 
+            "IMG_{0}.jpg",
+            @"IMG_\d\d\d\d_{0}\d{{3}}.MOV",
+            "PANO_{0}.jpg",
+            @"PH{0}-\d{{3}}.JPG",
+            "VID_{0}.3gp", 
+            "IMG_{0}_HDR.jpg",
+            @"IMG_{0}_\d.jpg",
+            @"IMG_{0}_\d{{3}}.jpg",
+            @"IMG_{0}_BURST\d.jpg",
+            @"\d{{10}}_{0}\d{{3}}.mp4",
+            String.Format(@"{0}_{{0}}\d{{{{3}}}}.mp4", GUID_MASK),
+            @"Screenshot_{0}-\d{{3}}_.*.png",
+        };
+
+        private readonly List<Tuple<string, Regex>> _sequences;
+
+        public TimeStampParser() {
+            _sequences = new List<Tuple<string, Regex>>();
+            foreach(var mask in MASKS) {
+                var tempResult = String.Format(mask, TIMESTAMP_MASK);
+                _sequences.Add(Tuple.Create(mask, new Regex(String.Format(mask, TIMESTAMP_MASK))));
+            }
+        }
+
+        public bool RecognizeStampFormat(string fileName, out DateTime fileDate) {
+            fileDate = DateTime.MinValue;
+            foreach (var sequence in _sequences) {
+                var match = sequence.Item2.Match(fileName);
+                if (match.Success)
+                {
+                    fileDate = ComposeDate(match);
+                    // CheckDateAgainstName(fileName, sequence.Item1, fileTime);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static DateTime ComposeDate(Match match)
+        {
+            var year = Convert.ToInt32(match.Groups["y"].Value);
+            var month = Convert.ToInt32(match.Groups["month"].Value);
+            var day = Convert.ToInt32(match.Groups["d"].Value);
+            var hour = Convert.ToInt32(match.Groups["h"].Value);
+            var minute = Convert.ToInt32(match.Groups["m"].Value);
+            var second = Convert.ToInt32(match.Groups["s"].Value);
+
+            var fileTime = new DateTime(year, month, day, hour, minute, second);
+            return fileTime;
+        }
+
+        private static void CheckDateAgainstName(string fileName, string mask, DateTime fileTime)
+        {
+            var stampFormat = 
+                $"{fileTime.Year}{fileTime.Month:d2}{fileTime.Day:d2}_{fileTime.Hour:d2}{fileTime.Minute:d2}{fileTime.Second:d2}";
+            var checkName = String.Format(mask, stampFormat);
+
+            if (checkName != fileName)
+            {
+                throw new InvalidDataException($"Parsed name does not match original name: {fileName} - {checkName}");
+            }
+        }
+
+        public static string FileStampFromDate(DateTime fileTime) {
+            return String.Format(TIMESTAMP_FORMAT, fileTime.Year, fileTime.Month, fileTime.Day, 
+                fileTime.Hour, fileTime.Minute, fileTime.Second);
+        }
+    }
+}
